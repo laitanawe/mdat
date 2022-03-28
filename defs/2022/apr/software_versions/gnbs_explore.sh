89ab17d22a7b1420c92ac79c7cc9bee15503baa5
Olaitan I. Awe <laitanawe@gmail.com>
2022-03-28 07:47:47 +0100

    Add new scripts

--- defs/2022/apr/software_versions/gnbs_explore.sh
+++ defs/2022/apr/software_versions/gnbs_explore.sh
@@ -13,7 +13,7 @@
   fastqc --version
   echo -n "STAR: "; STAR --version
   R --version | head -n 1
-  echo -n "Bioconductor:"; Rscript -e 'BiocManager::version()' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g' ## Display the Bioconductor version (formatted)
+  echo -n "Bioconductor:"; Rscript -e 'BiocManager::version()' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g' ## Display the Bioconductor vers$
   echo -n "ggplot2:"; Rscript -e 'packageVersion("ggplot2")' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g' ## Display the ggplot2 version
 #  echo -n "GAPIT:"; Rscript -e 'packageVersion("GAPIT3")' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g'
   echo -n "BLINK:"; Rscript -e 'packageVersion("BLINK")' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g'
@@ -24,8 +24,10 @@
   featureCounts -v 2>&1 | sed '/^$/d'
   echo -n "edgeR:"; Rscript -e 'packageVersion("edgeR")' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g' ## Display the edgeR version
   echo -n "limma:"; Rscript -e 'packageVersion("limma")' | sed 's/\[1]//' | sed 's/‘//g' | sed 's/’//g' ## Display the limma version
-  htseq-count -h | tail -1 | sed 's/^Public License v3. Part of the //'
-  gatk --version 2>&1 | head -3
+  echo -n "Nextflow: "; sudo nextflow -version | head -3 | tail -1 | sed 's/^ *//'
+#  htseq-count -h | tail -1 | sed 's/^Public License v3. Part of the //'
+# GATK
+  gatk --version 2>&1 | head -3 # | sed 's/[^Genome]*//'
   echo -n "snakemake: "; snakemake --version
-  echo -n "trim_galore: "; trim_galore --version | tail -4 | head -1
-  echo "BWA: "; bwa 2>&1 | head -3 | tail -1
+  echo -n "BWA: "; bwa 2>&1 | head -3 | tail -1
+  echo -n "trim_galore: "; trim_galore --version | tail -4 | head -1 | sed 's/^ *//'
